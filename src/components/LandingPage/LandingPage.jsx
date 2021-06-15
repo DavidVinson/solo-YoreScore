02b72a73936cda67bc43e2b450739080d54e6307
David Vinson <davidvinson018@gmail.com>
2021-06-15 12:05:48 -0500

    added bootstrap container to landing page for responsive form

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,57 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Battles fought, legends made');
+  const [heading, setHeading] = useState('Games played...players paid');
   const history = useHistory();
 
   const onLogin = (event) => {
     history.push('/login');
   };
 
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>{heading}</h2></Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Yore Score tracks the points awarded for each player during a game of Bingo, Bango, Bongo.
+            Point totals and payout have never been easier! Never forget who won with Yore Score!</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <RegisterForm />
+          <center>
+          <h4>Already a Member?</h4>
+          <Button size="sm" onClick={onLogin}>
+            Login
+          </Button>
+          </center>
+        </Col>
+      </Row>
+
+    </Container>
+  );
+
+
+}
+
+export default LandingPage;
+
+/*
+ORIGINAL
   return (
     <div className="container">
       <h2>{heading}</h2>
@@ -34,6 +73,4 @@ function LandingPage() {
       </div>
     </div>
   );
-}
-
-export default LandingPage;
+*/
